8c5649af19519233446cfa234e728f3ed36d4488
dikshagharde <117832798+dikshagharde@users.noreply.github.com>
2023-05-03 22:31:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 function fibonacci(num) {
-
-  let fib = [0, 1];
-  
-  for (let i = 2; i <= n; i++) {
-    fib[i] = fib[i - 1] + fib[i - 2];
-  }
-
-  return fib;
-	
+if (num === 1) return 0;
+else if (num === 2) return 1;
+num -= 2;
+var num1 = 0;
+var num2 = 1;
+var sum;
+var i = 0;
+for (i = 0; i < num; i++) {
+sum = num1 + num2;
+num1 = num2;
+num2 = sum;
+}
+return num2;
 }
 
 
