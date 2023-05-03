0aba76ca875bdc549fb01b4471c1024c43511443
dikshagharde <117832798+dikshagharde@users.noreply.github.com>
2023-05-03 22:28:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 function fibonacci(num) {
-// your code here
-	let a=0,b=1,c=0,i=3;
-while(i<=num){  
+
+  let fib = [0, 1];
+  
+  for (let i = 2; i <= n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+
+  return fib;
 	
-	c=a+b;  
-	a=b;
-     b=c;
-	i++;
 }
-	return c;  
-}
-let n=prompt("Enter n terms");
-let last=fibonacci(n);
-alert(last);
+
+
 module.exports = fibonacci;
